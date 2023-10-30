9d1a640816b0b671166a24a747502b5d7869cc69
mar1cruz <ddovidovich@list.ru>
2023-10-30 21:45:03 +0300

    add new paper

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,16 @@
 const connectBtn = document.querySelector(".input__button");
 const inputs = document.querySelectorAll("input");
+const burgerMenu = document.querySelector(".burger__menu");
+const header = document.querySelector(".header");
+const body = document.querySelector("body");
 
 connectBtn.addEventListener("click", () => {
   inputs.forEach((input) => {
     input.value = "";
   });
+});
+
+burgerMenu.addEventListener("click", () => {
+  header.classList.toggle("active");
+  body.classList.toggle("_lock");
 });
